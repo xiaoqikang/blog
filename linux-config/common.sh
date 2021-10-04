03bd52a5218609f0175e40b09c1338f484388698
ChenXiaoSong <chenxiaosongemail@foxmail.com>
2021-10-04 12:57:24 +0800

    更新笔记

---
+++ linux-config/common.sh
@@ -0,0 +1,18 @@
+array=(.vimrc .emacs .bash_profile .gitconfig)
+
+dst_dir=$1
+if [ -z "$dst_dir" ]
+then
+	dst_dir=$HOME
+fi
+
+for element in ${array[@]}
+do
+	src=$PWD/$element
+	dst=$dst_dir/$element
+
+	echo $src
+	echo $dst
+	rm $dst
+	$cmd $src $dst
+done
